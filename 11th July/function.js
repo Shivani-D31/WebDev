7460cd84acb22af18f375987edd8e70a52f900ce
Shivani-D31 <debnathshivani31@gmail.com>
2023-07-11 15:57:20 +0530

    JavaScript

---
+++ 11th July/function.js
@@ -0,0 +1,21 @@
+// function function_name(){ --> function defination
+
+//     //code -->function body
+// }
+
+// function add() {
+//   var num1 = parseInt(prompt("Enter first number"));
+//   var num2 = parseInt(prompt("Enter second number"));
+//   var sum = num1 + num2;
+//   console.log(sum);
+//   document.getElementById("result").innerHTML=sum;
+// }
+
+// function add(a,b){
+//     return a*b;
+// }
+
+var text1="REsult";
+var text2="script";
+var result=text1.concat(7+5);
+console.log(result);
---
+++ 11th July/operators.js
@@ -0,0 +1,61 @@
+//arithmetic
+// +,-,*,**,/,%,++,--
+// let a=10;
+// let b=2;
+// console.log(a+b); //12
+// console.log(a-b); //8
+// console.log(a*b); //20
+// console.log(a**b);//100
+// console.log(a/b);//5
+// console.log(a%b);//0
+// console.log(a++);//10
+// console.log(++a);//12
+// console.log(++b);//3
+// console.log(b++);//3
+// console.log(b);//4
+
+// var num1=parseInt(prompt("Enter first number"));
+// var num2=parseInt(prompt("Enter second number"));
+// prompt->input in alert form
+// note: js takes everything in string format that why we have used parseInt for converting string into int
+// var sum=num1+num2;
+// console.log(sum);
+
+//assigment
+
+// let a=5;
+// a+=1; ->shorthand
+// a=a+1;
+// console.log(a);
+
+
+//comaparison or relational->boolean -true and false
+// var a=31;
+// var b=20;
+// console.log(a==b); //false 
+// console.log(a!=b);// true
+// console.log(a>=b);//true
+// console.log(a>b);//true
+// console.log(a<=b);//false
+// console.log(a<b);//false
+// console.log(a===b); //false
+
+// == ->compare the value 
+// === -> commpare the value as well as datatype
+
+//logical -> &&(and) || (or) !(not)
+// var c=10;
+// var d=15;
+// console.log((c>9) &&(d>14));
+// console.log((c>45) || (d>14));
+// console.log(!(false));
+// console.log(!((c>9) &&(d>14)));
+
+// ternary operator
+
+// (condition)? true :false
+// var n1=4;
+// var result= (n1>22)?"yes":"no";
+// console.log(result);
+//  nested ternary
+// var result=(n1>20)?(Condition? true:false):(Condition?true:false)
